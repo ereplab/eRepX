73da4b107d4751372c02dd79b562bb474aca9e11
driversti <github@seniorjava.dev>
2023-04-15 16:41:15 +0200

    init

---
+++ eRepX.userscript.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name             eRepX
+// @version          0.0.1
+// @description      An attempt to add helpful extensions to our game
+// @author           driversti
+// @updateURL
+// @downloadURL
+// @run-at           document-end
+// @match            https://www.erepublik.com/*
+// @grant            GM_addStyle
+// @grant            GM_getResourceURL
+// @grant            GM_getResourceText
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  console.log("It works");
+})();
